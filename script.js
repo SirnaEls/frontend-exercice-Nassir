c19ff3f9a9106c6cf82d408125adc64a32bb224d
SirnaEls <nassir.elabbassi@yahoo.fr>
2022-07-25 18:11:10 +0200

    Exercice finished

--- script.js
+++ script.js
@@ -1,14 +1,54 @@
 function getAPI() {
   let inputCommune = document.getElementById("input-adresse").value;
   let displayCommune = document.getElementById("display-commune")
+  displayCommune.innerHTML = ""
 
   fetch(`https://api-adresse.data.gouv.fr/search/?q=${inputCommune}`)
     .then((res) => res.json())    
     .then((data) =>JSON.stringify(data.features.forEach(element => {
         //displayCommune.textContent += " "+ element.properties.city +   
-        displayCommune.textContent += ` ${element.properties.city}\n(${element.properties.context})`
+        displayCommune.innerHTML += ` ${element.properties.city} (${element.properties.context})<br/>`
         
     })))
     
 }
 
+function getDepartment(){
+    let getDepartment = document.getElementById("department-select");
+    let displayCommunesInDepartment = document.getElementById("display-commune-department")
+
+    fetch(`https://geo.api.gouv.fr/departements`)
+     .then((res) => res.json())    
+     .then((data) => data.forEach(element =>{
+       // console.log(element)
+        var opt = document.createElement('option');
+        opt.value = element.code ;
+        opt.innerHTML = element.nom;
+        getDepartment.appendChild(opt);
+     }))
+}
+
+getDepartment()
+
+
+
+let displayCommunesInDepartment = document.getElementById("display-commune-department")
+
+function getCommunesByDepartment(){
+
+    var departmentSelector = document.getElementById("department-select");
+    departmentSelector.addEventListener("change", function(){
+        displayCommunesInDepartment.textContent = ""
+        var test = departmentSelector.value
+        console.log(test)
+        fetch(`https://geo.api.gouv.fr/departements/${test}/communes`)
+        .then((res) => res.json())    
+        .then((data) => data.forEach (element => {
+            //console.log(element)
+            displayCommunesInDepartment.innerHTML += `${element.nom}<br/>`
+        }))
+    })
+
+}
+
+getCommunesByDepartment()
